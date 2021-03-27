4998eabfdb8c5e0cd5c100e4f95ecba9072bad01
Ana Gabriele <57578764+Anagabsoares@users.noreply.github.com>
2021-03-27 11:44:52 -0400

    git commit setting drinks

--- my-react-app/src/views/setting-drinks.js
+++ my-react-app/src/views/setting-drinks.js
@@ -62,7 +62,7 @@ class SettingDrinks extends React.Component{
     
       post() {
             let postAdrink= async () => {
-                const serverUrl = "https://coffeeshopcreepy.herokuapp.com";
+                const serverUrl = 'https://coffeeshopcreepy.herokuapp.com';
                 const { getAccessTokenSilently } = this.props.auth0;
                 const inputData= {title: this.state.title,
                             recipe:{color: this.state.color,
