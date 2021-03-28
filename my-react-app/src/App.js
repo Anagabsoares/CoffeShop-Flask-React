06ae0d1b068811466273df013c75167f80e80a62
Ana Gabriele <57578764+Anagabsoares@users.noreply.github.com>
2021-03-27 22:17:16 -0400

    commit ser-drinks

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { NavBar, Footer, Loading } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
 import DrinkMenu from "./views/drink-menu"
-import SettingDrinks from "./views/setting-drinks";
+import SettingDrinks from "./views/set-drinks";
 import ProtectedRoute from "./auth/protected-route";
 
 import "./App.css";
